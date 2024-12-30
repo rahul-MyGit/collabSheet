e60e259942335817955dc18703978516cddd6c79
Rahul <rahul.mymail1@gmail.com>
2024-12-31 05:04:01 +0530

    feat: add sheet controller

--- apps/http-backend/src/controller/sheet.controller.ts
+++ apps/http-backend/src/controller/sheet.controller.ts
@@ -1,36 +1,181 @@
+import prisma from "@/db";
 import { ApiResponse } from "@/lib/ApiResponse";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
-export const getAllDocument = (req: Request, res: Response) => {
+export const getAllDocument = async (req: Request, res: Response) => {
     try {
-        
+        const userId = req.userId;
+
+        const documents = await prisma.document.findMany({
+            where: {
+                OR: [
+                    { ownerId: userId },
+                    {
+                        collaborators: {
+                            some: {
+                                userId: userId
+                            }
+                        }
+                    }
+                ],
+                deletedAt: null
+            },
+            include: {
+                owner: {
+                    select: {
+                        username: true,
+                        email: true
+                    }
+                },
+                collaborators: {
+                    include: {
+                        user: {
+                            select: {
+                                username: true,
+                                email: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        res.status(200).json(documents);
+        return
     } catch (error) {
         ApiResponse(res, 500, false, "Internal server error");
         return;
     }
 }
 
-export const createNewDocument = (req: Request, res: Response) => {
+export const createNewDocument = async (req: Request, res: Response) => {
     try {
-        
+        const { title } = req.body;
+        const userId = req.userId;
+        if (!userId) {
+            ApiResponse(res, 400, false, "User ID is required");
+            return;
+        }
+
+        const document = await prisma.document.create({
+            data: {
+                title,
+                ownerId: userId,
+                collaborators: {
+                    create: {
+                        userId: userId,
+                        role: 'OWNER'
+                    }
+                }
+            },
+            include: {
+                owner: {
+                    select: {
+                        username: true,
+                        email: true
+                    }
+                },
+                collaborators: {
+                    include: {
+                        user: {
+                            select: {
+                                username: true,
+                                email: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        res.status(201).json(document);
+        return;
     } catch (error) {
         ApiResponse(res, 500, false, "Internal server error");
         return;
     }
 }
 
-export const getDocument = (req: Request, res: Response) => {
+export const getDocument = async (req: Request, res: Response) => {
     try {
-        
+        const { id } = req.params;
+        const userId = req.userId;
+
+        const document = await prisma.document.findFirst({
+            where: {
+                id,
+                OR: [
+                    { ownerId: userId },
+                    {
+                        collaborators: {
+                            some: {
+                                userId
+                            }
+                        }
+                    }
+                ],
+                deletedAt: null
+            },
+            include: {
+                owner: {
+                    select: {
+                        username: true,
+                        email: true
+                    }
+                },
+                collaborators: {
+                    include: {
+                        user: {
+                            select: {
+                                username: true,
+                                email: true
+                            }
+                        }
+                    }
+                },
+                version: {
+                    orderBy: {
+                        createdAt: 'desc'
+                    },
+                    take: 1
+                }
+            }
+        });
+
+        if (!document) {
+            return res.status(404).json({ message: 'Document not found' });
+        }
+
+        res.status(201).json(document)
     } catch (error) {
         ApiResponse(res, 500, false, "Internal server error");
         return;
     }
 }
 
-export const deleteDocument = (req: Request, res: Response) => {
+export const deleteDocument = async (req: Request, res: Response) => {
     try {
-        
+        const { id } = req.params;
+        const userId = req.userId;
+
+        const document = await prisma.document.findFirst({
+            where: {
+                id,
+                ownerId: userId,
+                deletedAt: null
+            }
+        });
+
+        if (!document) {
+            return res.status(404).json({ message: 'Document not found or unauthorized' });
+        }
+
+        await prisma.document.update({
+            where: { id },
+            data: { deletedAt: new Date() }
+        });
+
+        res.status(200).json({ message: 'Document deleted successfully' });
+        return;
     } catch (error) {
         ApiResponse(res, 500, false, "Internal server error")
         return;
